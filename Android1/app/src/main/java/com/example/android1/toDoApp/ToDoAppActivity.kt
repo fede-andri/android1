9e837b51a84b213d1e3dba57332d0c24ee571828
fede-andri <fede_andrijasevich@hotmail.com>
2025-09-22 08:29:58 -0300

    Se cambio estilo de fondo a la app de TODO

--- Android1/app/src/main/java/com/example/android1/toDoApp/ToDoAppActivity.kt
+++ Android1/app/src/main/java/com/example/android1/toDoApp/ToDoAppActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android1.toDoApp
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.android1.R
@@ -17,5 +20,17 @@ class ToDoAppActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(
+                ContextCompat.getColor(this, R.color.todo_background_todo_app));
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(0);
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.todo_background_todo_app));
+            // Iconos oscuros
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+        }
+
     }
 }
